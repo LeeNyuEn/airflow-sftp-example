be2ed8d11e54f0dbc0678c4862f96f507c0e463c
NguyenLe <nguyenle@it>
2024-03-19 03:34:11 +0700

    pipeline to transfer file in sftp servers

--- dags/airflow_example/lib/pipeline.py
+++ dags/airflow_example/lib/pipeline.py
@@ -1,10 +1,13 @@
 import abc
 from typing import Dict, List, Union
 
+import time
+
 from airflow_example.lib.source import DataSourceManagerFactory
 from airflow_example.lib.target import DataTargetManagerFactory
 from airflow_example.lib.transform.base import DataTransformer
 from airflow_example.utils import list_util
+from config import settings, current_folder
 
 
 class DataPipeline(abc.ABC):
@@ -48,6 +51,13 @@ class SftpFileTransferPipeline(DataPipeline):
     This class facilitates secure file transfer to and from SFTP servers.
     """
 
+    _CHUNK_SIZE = 32768
+    _RETRY_DELAY = 10
+    _MAX_RETRIES = 3
+    _CHUNK_SIZE = settings.sftp_default_config.chunk_size
+    _RETRY_DELAY = settings.sftp_default_config.retry_delay
+    _MAX_RETRIES = settings.sftp_default_config.max_retries
+
     def __init__(
         self,
         source_config: Dict,
@@ -62,11 +72,46 @@ class SftpFileTransferPipeline(DataPipeline):
         else:
             print(f"No directory needed to create")
 
+    def __transfer_files(
+        self,
+        source_file_path: str,
+        target_file_path: Union[str, None] = None,
+    ):
+        target_file_path = target_file_path if target_file_path else source_file_path
+        retries = 0
+        while True:
+            try:
+                chunks = self.source_manager.fetch_data(
+                    source_file_path, SftpFileTransferPipeline._CHUNK_SIZE
+                )
+                self.target_manager.ingest_data(target_file_path, chunks)
+                break  # Break the loop if transfer is successful
+            except Exception as e:
+                print(f"Error: {e}")
+                retries += 1
+                if retries == SftpFileTransferPipeline._MAX_RETRIES:
+                    print("Max retries reached. Could not complete the operation.")
+                    raise Exception("Max retries reached")
+                print(f"Retrying in {SftpFileTransferPipeline._RETRY_DELAY} seconds...")
+                time.sleep(SftpFileTransferPipeline._RETRY_DELAY)
+                continue
+
     def run(self):
+        self.source_manager.get_sftp_client()
+        self.target_manager.get_sftp_client()
+
         source_file_paths, source_directory = self.source_manager.list_files()
         target_file_paths, target_directory = self.target_manager.list_files()
         files_to_put = list_util.not_in(source_file_paths, target_file_paths)
         directory_to_create = list_util.not_in(source_directory, target_directory)
-        print(files_to_put)
-        print(directory_to_create)
+
+        print(f"Files need to put: {files_to_put}")
+        print(f"Directory need to create: {directory_to_create}")
+
         self.__create_directory(directory_to_create)
+
+        for file in files_to_put:
+            self.__transfer_files(source_file_path=file)
+
+        self.source_manager.close_connection()
+        self.target_manager.close_connection()
--- dags/airflow_example/lib/target/sftp.py
+++ dags/airflow_example/lib/target/sftp.py
@@ -1,8 +1,16 @@
+from typing import Iterator
+
 from airflow_example.lib.target.base import DataTargetManager
 from airflow_example.lib.connections.sftp import SftpManager
 
 
 class SftpDataTargetManager(DataTargetManager, SftpManager):
-    def ingest_data(self):
-        # Logic to ingest data into Google Cloud Storage
-        print("Ingesting data into SFTP Server")
+    def ingest_data(
+        self,
+        destination_file_path: str,
+        chunks: Iterator[bytes],
+    ) -> None:
+        with self.sftp_client.open(destination_file_path, "ab") as destination_file:
+            destination_file.set_pipelined(True)
+            for i, chunk in enumerate(chunks):
+                destination_file.write(chunk)
